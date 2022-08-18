30398633553332e989b30682d2e47d6d301e5e79
cruzandersom <cruz.andersom@gmail.com>
2022-08-18 20:42:04 -0300

    primeira_aula
    Dados da primeira aula

---
+++ src/Programa.java
@@ -0,0 +1,21 @@
+public class Programa {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Ola mundo");
+		
+		int a = 0; 
+		int b = 0;
+		
+		b = b + 20;
+		
+		int c; 
+		c = a + b;
+		System.out.println("O valor de c é:" + c);
+		
+
+		
+	}
+	
+}
+
